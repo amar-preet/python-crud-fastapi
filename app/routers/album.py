8e54fddb1355f386586406204bf45ea8684af709
amar-preet <amarpreet.singh1985@gmail.com>
2022-10-27 00:47:13 -0700

    update route

--- app/routers/album.py
+++ app/routers/album.py
@@ -6,21 +6,21 @@ from app.services import album_service as service
 router = APIRouter(prefix="/albums")
 
 
-@router.get("/albums/")
+@router.get("/")
 async def get_albums():
     return service.get_albums()
 
 
-@router.get("/albums/{album_id}")
+@router.get("/{album_id}")
 async def get_album_by_id(album_id: str, q: Union[str, None] = None):
     return service.get_album_by_id(album_id=album_id)
 
 
-@router.post("/albums/")
+@router.post("/")
 async def create_album(album: Album):
     return album
 
 
-@router.delete("/albums/")
+@router.delete("/")
 async def delete_album(album: Album):
     return album
